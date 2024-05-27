d23b0eec2716e489bd05fb8cd1e3f01dbbea3c67
KrishnappaL <lakshmihk2003@gmail.com>
2024-05-27 14:58:53 +0530

    build_run updated

--- build_run.sh
+++ build_run.sh
@@ -3,6 +3,14 @@
 git clone https://github.com/KrishnappaL/App.git
 
 cd App
+BUILD_TIMESTAMP=$( date '+%F_%H:%M:%S')
+echo "Time: $BUILD_TIMESTAMP"
+REMOTE=origin
+BRANCH=main
+git fetch
+if [["$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]];  then
+        #Run your script
+git pull
 
 #Build the Docker image
 docker build -t lakshmi039/my_web_app:latest  .
@@ -19,3 +27,7 @@ docker container rm my_web_app_container
 
 #run the docker container
 docker run -d -p 8081:80 --name my_web_app_container lakshmi039/my_web_app:latest
+else
+        echo "No change on $REMOTE/$BRANCH"
+fi
+
