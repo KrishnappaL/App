840508f3dd07b1e36dd25679b9bc2c3ec6d6ba21
Lakshmi <142583809+KrishnappaL@users.noreply.github.com>
2024-05-30 13:16:31 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-REMOTE=origin
-BRANCH=main
-git fetch
-if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; then
-	echo "change found on remote"
-else
-	echo "no change"
-fi
