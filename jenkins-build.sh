738b7346046a1791a1254e030ce0503a69564fb3
Lakshmi <142583809+KrishnappaL@users.noreply.github.com>
2024-06-10 14:21:57 +0530

    Update jenkins-build.sh

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -8,5 +8,7 @@ echo "Time: $BUILD_TIMESTAMP"
 pwd
 REMOTE=origin
 BRANCH=main
+docker stop my_web_app_container
+docker container rm my_web_app_container 
 docker build -t "$TAG" -t "$LATEST" --build-arg VERSION="$VERSION" --build-arg BUILD_TIMESTAMP="$BUILD_TIMESTAMP" . 
-docker run -d -p 8082:80 --name my_web_app lakshmi039/my_web_app:"$VERSION"
+docker run -d -p 8082:80 --name my_web_app_container lakshmi039/my_web_app:"$VERSION"
